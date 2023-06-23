38bb75775939f952a8d4edb6a8584e0b71db234b
James Best <james.best@breedr.co>
2023-06-23 16:33:27 +0100

    Initial setup programmatic uis

---
+++ CardWorkoutProgramatic/CardSelectionVC.swift
@@ -0,0 +1,29 @@
+//
+//  CardSelectionVC.swift
+//  CardWorkoutProgramatic
+//
+//  Created by James Best on 23/06/2023.
+//
+
+import UIKit
+
+class CardSelectionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemCyan
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
